4d46337b9774d2cb83d07224ee70179c3c475b26
cricket-sama <156969289+cricket-sama@users.noreply.github.com>
2024-12-17 17:20:26 -0500

    week 7 completed

--- src/App.jsx
+++ src/App.jsx
@@ -25,11 +25,19 @@ function App() {
     setTodoList([...todoList, newTodo]);
   };
 
+  function removeTodo(id) {
+  const newTodoList = todoList.filter(
+    (todo) => todo.id !== id
+  );
+
+  setTodoList(newTodoList);
+}
+
   return (
     <>
       <h1>Todo List</h1>
       <AddTodoForm onAddTodo={addTodo} />
-      <TodoList todoList={todoList} />
+      <TodoList todoList={todoList} onRemoveTodo={removeTodo} />
     </>
   );
 }
